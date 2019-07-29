6eacbb903803d880029c49824ea2d27db883151e
Yuri Duarte <yuriduartedev@gmail.com>
2019-07-29 19:30:13 -0300

    Adiciona campo de nota e link dos parques do Rio

--- web-crawler-trip.js
+++ web-crawler-trip.js
@@ -1,20 +1,41 @@
 let axios = require("axios");
 let cheerio = require("cheerio");
 let fs = require("fs");
-let urls = Array(10).fill().map((_, i) => {
-    if (i === 0) {
-      return 'https://www.tripadvisor.com.br/Attraction_Review-g303506-d11725544-Reviews-AquaRio-Rio_de_Janeiro_State_of_Rio_de_Janeiro.html'
-    } else {
-      return `https://www.tripadvisor.com.br/Attraction_Review-g303506-d11725544-Reviews-or${i}0-AquaRio-Rio_de_Janeiro_State_of_Rio_de_Janeiro.html`
-    }
+
+let urls = Array(471).fill().map((_, i) => {
+  // Link de Teste
+  // return 'https://www.tripadvisor.com.br/Attraction_Review-g303506-d11840851-Reviews-Centro_de_Visitantes_Paineiras-Rio_de_Janeiro_State_of_Rio_de_Janeiro.html'
+
+  // AquaRio
+  if (i === 0) {
+    return 'https://www.tripadvisor.com.br/Attraction_Review-g303506-d11725544-Reviews-AquaRio-Rio_de_Janeiro_State_of_Rio_de_Janeiro.html'
+  } else {
+    return `https://www.tripadvisor.com.br/Attraction_Review-g303506-d11725544-Reviews-or${i}0-AquaRio-Rio_de_Janeiro_State_of_Rio_de_Janeiro.html`
+  }
+
+  // Paineiras
+  // if (i === 0) {
+  //   return 'https://www.tripadvisor.com.br/Attraction_Review-g303506-d11840851-Reviews-Centro_de_Visitantes_Paineiras-Rio_de_Janeiro_State_of_Rio_de_Janeiro.html'
+  // } else {
+  //   return `https://www.tripadvisor.com.br/Attraction_Review-g303506-d11840851-Reviews-or${i}0-Centro_de_Visitantes_Paineiras-Rio_de_Janeiro_State_of_Rio_de_Janeiro.html`
+  // }
+
+  // RioZoo
+  // if (i === 0) {
+  //   return 'https://www.tripadvisor.com.br/Attraction_Review-g303506-d2352236-Reviews-RioZoo_Rio_de_Janeiro_s_Zoo-Rio_de_Janeiro_State_of_Rio_de_Janeiro.html'
+  // } else {
+  //   return `https://www.tripadvisor.com.br/Attraction_Review-g303506-d2352236-Reviews-or${i}0-RioZoo_Rio_de_Janeiro_s_Zoo-Rio_de_Janeiro_State_of_Rio_de_Janeiro.html`
+  // }
 });
 
 Promise.all(urls.map(extractData)).then(allResults => {
     let result = [].concat.apply([], allResults);
     let file = JSON.stringify(result, null, 4);
     // let file = result.join('\n')
-    fs.writeFile("data.json", file, err =>
-        console.log("File successfully written!")
+    // console.log(result[0].rating);
+
+    fs.writeFile("data2.json", file, err =>
+      console.log("Arquivo salvo com sucesso!")
     );
 })
 
@@ -46,12 +67,34 @@ function extractData(url) {
             .find(".prw_reviews_stay_date_hsx")
             .text()
             .replace('Data da experiência:', '')
-            .trim()
+            .trim(),
+          rating: $(this)
+            .find(".ui_bubble_rating")
+            .attr('class')
+            .trim(),
         };
-        data.push(review);        
-        // data.push(`
-        //     ${review.title},${review.description},${review.location},${review.publication},${review.date}
-        // `.trim());
+        
+        switch (review.rating) {
+          case "ui_bubble_rating bubble_10":
+            review.rating = 1
+            break
+          case "ui_bubble_rating bubble_20":
+            review.rating = 2
+            break
+          case "ui_bubble_rating bubble_30":
+            review.rating = 3
+            break
+          case "ui_bubble_rating bubble_40":
+            review.rating = 4
+            break
+          case "ui_bubble_rating bubble_50":
+            review.rating = 5
+            break
+          default:
+            // console.log('Nota inválida')
+        }
+
+        data.push(review);       
       });
       console.log('[200]', url);
     } else {
